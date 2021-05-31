d8690f8dd5ca869dd03f0767fa6cc975a6707d1a
mtosatti33 <m_tosatti@yahoo.com.br>
2021-05-31 07:20:58 -0300

    commit #3

--- ufrmmain.pas
+++ ufrmmain.pas
@@ -16,14 +16,19 @@ type
   TfrmMain = class(TForm)
     actCloseApplication: TAction;
     actCallRosterForm: TAction;
+    actCallDepthChartForm: TAction;
+    actCallInjuriesForm: TAction;
+    actCallTradesForm: TAction;
+    actMinimize: TAction;
     actMenu: TAction;
     actions: TActionList;
-    BCButton1: TBCButton;
+    btnClose: TBCButton;
     BCButton2: TBCButton;
     BCButton3: TBCButton;
     BCButton4: TBCButton;
     BCButton5: TBCButton;
     BCButton6: TBCButton;
+    btnMinimize: TBCButton;
     images: TImageList;
     Panel1: TPanel;
     pnlParent: TPanel;
@@ -31,9 +36,13 @@ type
     pnlMenu: TPanel;
     Panel3: TPanel;
     Panel4: TPanel;
+    procedure actCallDepthChartFormExecute(Sender: TObject);
+    procedure actCallInjuriesFormExecute(Sender: TObject);
     procedure actCallRosterFormExecute(Sender: TObject);
+    procedure actCallTradesFormExecute(Sender: TObject);
     procedure actCloseApplicationExecute(Sender: TObject);
     procedure actMenuExecute(Sender: TObject);
+    procedure actMinimizeExecute(Sender: TObject);
     procedure Button1Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormShow(Sender: TObject);
@@ -59,7 +68,7 @@ end;
 
 procedure TfrmMain.actCallRosterFormExecute(Sender: TObject);
 begin
-  if frmRoster = nil then
+  if not Assigned(frmRoster) then
   begin
     frmRoster := TfrmRoster.Create(Self);
     frmRoster.Align := alClient;
@@ -68,12 +77,35 @@ begin
   end;
 end;
 
+procedure TfrmMain.actCallTradesFormExecute(Sender: TObject);
+begin
+  //TODO: Fazer o form de Trades (se houver)
+  ShowMessage('In Future');
+end;
+
+procedure TfrmMain.actCallDepthChartFormExecute(Sender: TObject);
+begin
+  //TODO: Fazer o form de Depth Chart(Jogadores nas suas posições)
+  ShowMessage('In Future');
+end;
+
+procedure TfrmMain.actCallInjuriesFormExecute(Sender: TObject);
+begin
+  //TODO: Fazer o form de Injuries (Lesões do FA)
+  ShowMessage('In Future');
+end;
+
 procedure TfrmMain.actMenuExecute(Sender: TObject);
 begin
   if pnlMenu.Width = FWidth then
     pnlMenu.Width := 0
   else
     pnlMenu.Width := FWidth;
+end;
+
+procedure TfrmMain.actMinimizeExecute(Sender: TObject);
+begin
+  Application.Minimize;
 end;
 
 procedure TfrmMain.Button1Click(Sender: TObject);
